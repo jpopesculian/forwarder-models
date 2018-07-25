b4b0eaabe3f9f7630fb1a221b18f3e134ecb1d83
Julian Popescu <jpopesculian@gmail.com>
2018-07-25 18:08:03 +0200

    text send update

--- lib/forwarder_models.rb
+++ lib/forwarder_models.rb
@@ -2,3 +2,4 @@ require 'active_record'
 
 require_relative 'models/text'
 require_relative 'models/text/receive'
+require_relative 'models/text/send'
---
+++ lib/models/text/send.rb
@@ -0,0 +1,17 @@
+module Models
+  class Text
+    class Send
+      def self.call(sms)
+        text = Text.find_or_create_by(guid: sms.id)
+        text.update(
+          twilio_id: sms.twilio_id,
+          received_time: sms.received_time,
+          sent_time: sms.sent_time,
+          from: sms.from,
+          to: sms.to,
+          body: sms.body
+        )
+      end
+    end
+  end
+end
